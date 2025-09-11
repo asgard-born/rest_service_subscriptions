64e917fa9b1db051ae0d60f2bc1c32f91d5c4726
Viktor Kapustin <v.kapustin@ll-games.com>
2025-09-11 18:03:04 +0300

    router decomposition

---
+++ pkg/api/router.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func CreateNewRouter(db *pgxpool.Pool) *gin.Engine {
+	h := Handler{
+		db: db,
+	}
+
+	router := gin.New()
+
+	subscriptions := router.Group("/subscriptions")
+	{
+		subscriptions.POST("/", h.CreateSubscription)
+		subscriptions.GET("/:id", h.GetSubscription)
+		subscriptions.PUT("/:id", h.UpdateSubscription)
+		subscriptions.DELETE("/:id", h.DeleteSubscription)
+		subscriptions.GET("/", h.ListSubscriptions)
+	}
+
+	router.GET("/subscriptions/summary", h.GetSubscriptionsSummary)
+
+	return router
+}
