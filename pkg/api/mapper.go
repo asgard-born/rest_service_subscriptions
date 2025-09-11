d51c5c274e2bd75c347252ce324013be97bae7a5
Viktor Kapustin <v.kapustin@ll-games.com>
2025-09-12 01:14:06 +0300

    +date formats, EndDate validation

--- pkg/api/mapper.go
+++ pkg/api/mapper.go
@@ -3,14 +3,19 @@ package api
 import "github.com/asgard-born/rest_service_subscriptions/pkg/db"
 
 func ToSubscriptionResponse(s db.Subscription) SubscriptionResponse {
+	var endDate string
+	if s.EndDate.Valid {
+		endDate = s.EndDate.Time.Format("01-2006")
+	}
+
 	return SubscriptionResponse{
 		ID:          s.ID,
 		ServiceName: s.ServiceName,
 		Price:       s.Price,
 		UserID:      s.UserID,
-		StartDate:   s.StartDate,
-		EndDate:     s.EndDate,
-		CreatedAt:   s.CreatedAt,
-		UpdatedAt:   s.UpdatedAt,
+		StartDate:   s.StartDate.Format("01-2006"),
+		EndDate:     endDate,
+		CreatedAt:   s.CreatedAt.Format("2006-01-02 15:04:05"),
+		UpdatedAt:   s.UpdatedAt.Format("2006-01-02 15:04:05"),
 	}
 }
