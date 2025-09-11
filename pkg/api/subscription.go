c5870b84af5be85fcc5159846016f0c75afdf5df
Viktor Kapustin <v.kapustin@ll-games.com>
2025-09-12 01:19:40 +0300

    dto subscription fixes

--- pkg/api/subscription.go
+++ pkg/api/subscription.go
@@ -1,14 +1,12 @@
 package api
 
-import "time"
-
 type SubscriptionResponse struct {
-	ID          string     `json:"id"`
-	ServiceName string     `json:"service_name"`
-	Price       float64    `json:"price"`
-	UserID      string     `json:"user_id"`
-	StartDate   time.Time  `json:"start_date"`
-	EndDate     *time.Time `json:"end_date,omitempty"`
-	CreatedAt   time.Time  `json:"created_at"`
-	UpdatedAt   time.Time  `json:"updated_at"`
+	ID          string `json:"id"`
+	ServiceName string `json:"service_name"`
+	Price       int64  `json:"price"`
+	UserID      string `json:"user_id"`
+	StartDate   string `json:"start_date"`
+	EndDate     string `json:"end_date"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
 }
