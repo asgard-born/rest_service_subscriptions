c04a26b7fbf8d648218b63327de5b39f6a74a05d
Viktor Kapustin <v.kapustin@ll-games.com>
2025-09-11 15:14:11 +0300

    logs

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package rest_service
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"time"
 )
@@ -14,14 +15,24 @@ func (s *Server) Run(port string, handler http.Handler) error {
 	s.httpServer = &http.Server{
 		Addr:           ":" + port,
 		Handler:        handler,
-		MaxHeaderBytes: 1 << 20, //1 MB
+		MaxHeaderBytes: 1 << 20, // 1 MB
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 	}
 
-	return s.httpServer.ListenAndServe()
+	log.Printf("starting server on port %s", port)
+	err := s.httpServer.ListenAndServe()
+
+	if err != nil && err != http.ErrServerClosed {
+		log.Printf("server error: %v", err)
+		return err
+	}
+
+	log.Println("server stopped gracefully")
+	return err
 }
 
 func (s *Server) Shutdown(ctx context.Context) error {
+	log.Println("shutting down server...")
 	return s.httpServer.Shutdown(ctx)
 }
