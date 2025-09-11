bdfb2b98d98a5d1fba9561a43695004a35b6ed9b
Viktor Kapustin <v.kapustin@ll-games.com>
2025-09-12 01:20:25 +0300

    bd subscription fixes

--- pkg/db/subscription.go
+++ pkg/db/subscription.go
@@ -1,14 +1,17 @@
 package db
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type Subscription struct {
-	ID          string     `db:"id"`
-	ServiceName string     `db:"service_name"`
-	Price       float64    `db:"price"`
-	UserID      string     `db:"user_id"`
-	StartDate   time.Time  `db:"start_date"`
-	EndDate     *time.Time `db:"end_date"`
-	CreatedAt   time.Time  `db:"created_at"`
-	UpdatedAt   time.Time  `db:"updated_at"`
+	ID          string       `db:"id"`
+	ServiceName string       `db:"service_name"`
+	Price       int64        `db:"price"`
+	UserID      string       `db:"user_id"`
+	StartDate   time.Time    `db:"start_date"`
+	EndDate     sql.NullTime `db:"end_date"`
+	CreatedAt   time.Time    `db:"created_at"`
+	UpdatedAt   time.Time    `db:"updated_at"`
 }
